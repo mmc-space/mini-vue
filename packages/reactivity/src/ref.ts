974940089ad469202345ce123f7dae97cc97986d
aiden <danyang.aiden@outlook.com>
2023-04-16 18:38:49 +0800

    feat: toRef toRefs proxyRefs

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,4 +1,4 @@
-import { isObject } from '@vue/shared'
+import { isArray, isObject } from '@vue/shared'
 import { reactive } from './reactive'
 import { trackEffects, triggerEffects } from './effect'
 // 变成响应式的
@@ -31,4 +31,49 @@ class RefImp {
 
 export function ref(value: any) {
   return new RefImp(value)
+}
+
+class ObjectRefImpl {
+  constructor(public object: any, public key: any) {}
+
+  get value() {
+    return this.object[this.key]
+  }
+  set value(newValue) {
+    this.object[this.key] = newValue
+  }
+}
+
+export function toRef(object: any, key: any) {
+  return new ObjectRefImpl(object, key)
+}
+
+export function toRefs(object: any) {
+  // 对象 or 数组
+  const result: any = isArray(object) ? new Array(object.length) : {}
+  
+  for (const key in object) {
+    result[key] = toRef(object, key)
+  }
+
+  return result
+}
+
+export function proxyRefs(object: any) {
+  return new Proxy(object, {
+    get(target, key, recevier) {
+      let r = Reflect.get(target, key, recevier)
+      // 判断r 是否是ref
+      return r.__v_isRef ? r.value : r
+    },
+    set(target, key, value, recevier) {
+      let oldValue = target[key]
+      if (oldValue.__v_isRef) {
+        oldValue.value = value
+        return true
+      } else {
+        return Reflect.set(target, key, value, recevier)
+      }
+    }
+  })
 }
